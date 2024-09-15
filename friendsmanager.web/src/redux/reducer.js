09713333b9f3c0697dd0f375894ee335a713d61b
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-15 20:12:10 +0100

    Added contacts reducer and page

---
+++ friendsmanager.web/src/redux/reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    contacts: [],
+    user: {},
+    loading: false
+};
+
+const contactReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
+
+export default contactReducer;

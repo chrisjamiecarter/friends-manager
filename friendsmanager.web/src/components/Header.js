ec7fba2f74e650ed45141482ff871d8acacf88ba
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-17 13:28:01 +0100

    Added logo to header

--- friendsmanager.web/src/components/Header.js
+++ friendsmanager.web/src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import logo from '../logo.svg';
 
 const Header = () => {
     return (
         <header className="bg-black">
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
-                <a href="#" className="flex -m-1.5 p-1.5">
-                    <img className="h-8 w-auto" src="TODO" alt="LOGO" />
-                    <span className="text-lg font-bold leading-6 text-slate-100">Friends Manager</span>
+                <a href="#" className="flex -m-1.5 p-1.5 items-center">
+                    <img className="h-8 w-auto rounded-full mx-1.5" src={logo} alt="Logo" />
+                    <span className="text-lg font-bold leading-6 text-slate-100 mx-1.5">Friends Manager</span>
                 </a>
                 </div>
                 <div className="lg:flex lg:gap-x-12">

0f4c1a1fa8cfc164cedd78beeea705348b1e7c45
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-19 19:32:32 +0100

    Updated Header

--- friendsmanager.web/src/components/Header.js
+++ friendsmanager.web/src/components/Header.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import logo from '../logo.svg';
 
 const Header = () => {
     return (
-        <header className="bg-black">
-            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-                <div className="flex lg:flex-1">
-                <a href="#" className="flex -m-1.5 p-1.5 items-center">
-                    <img className="h-8 w-auto rounded-full mx-1.5" src={logo} alt="Logo" />
-                    <span className="text-lg font-bold leading-6 text-slate-100 mx-1.5">Friends Manager</span>
-                </a>
-                </div>
-                <div className="lg:flex lg:gap-x-12">
-                <a href="#" className="text-sm font-semibold leading-6 text-slate-100">Contacts</a>
-                <a href="#" className="text-sm font-semibold leading-6 text-slate-100">Friends</a>
-                <a href="#" className="text-sm font-semibold leading-6 text-slate-100">Activities</a>
+        <header>
+            <nav className='bg-white shadow'>
+                <div className='container flex items-center justify-between p-6 mx-auto text-slate-800 capitalize'>
+                    <a href="#/top" className='flex'>
+                        <img className='rounded-full w-auto h-6 sm:h-7' src={logo} alt='Logo' />
+                        <h1 className='text-lg font-bold mx-2'>Friends Manager</h1>
+                    </a>
+                    <ul className="sm:flex lg:gap-x-12">
+                        <li className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform hover:border-blue-500 mx-1.5 sm:mx-6"><Link to="/">friends</Link></li>
+                        <li className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform hover:border-blue-500 mx-1.5 sm:mx-6"><Link to="/addfriend">add friend</Link></li>
+                        <li className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform hover:border-blue-500 mx-1.5 sm:mx-6">record activity</li>
+                    </ul>
                 </div>
             </nav>
         </header>

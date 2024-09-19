e05c6547884f3be5dcc193f387c91a4db956fb05
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-19 19:34:42 +0100

    Updated App Layout so Footer sticks to bottom and implemented routes

--- friendsmanager.web/src/App.js
+++ friendsmanager.web/src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import AddFriendForm from './components/AddFriendForm';
 import FriendList from './components/FriendList';
 import ErrorDisplay from './components/ErrorDisplay';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="app-container">
+    <div className="flex flex-col min-h-svh">
       <Header />
-      <ErrorDisplay />
-      <Routes>
-        <Route exact path="/" element={<FriendList />} />
-      </Routes>
+      <main className='w-full max-w-7xl my-4 mx-auto px-4 flex-grow'>
+        <ErrorDisplay />
+        <Routes>
+          <Route exact path="/" element={<FriendList />} />
+          <Route path="addfriend" element={<AddFriendForm />} />
+        </Routes>
+      </main>
+      <Footer />
       </div>
   );
 }

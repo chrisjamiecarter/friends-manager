be4831782468d7e0f5a5d5ba1cc0038bbcc027be
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-22 16:22:46 +0100

    Added Application Interfaces

---
+++ src/FriendsManager.Application/Repositories/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using FriendsManager.Domain.Entities;
+
+namespace FriendsManager.Application.Repositories;
+
+public interface ICategoryRepository
+{
+    Task CreateAsync(Category category);
+    Task DeleteAsync(Category category);
+    Task<IEnumerable<Category>> ReturnAsync();
+    Task<Category?> ReturnAsync(Guid id);
+    Task UpdateAsync(Category category);
+}
---
+++ src/FriendsManager.Application/Repositories/IFriendRepository.cs
@@ -0,0 +1,12 @@
+﻿using FriendsManager.Domain.Entities;
+
+namespace FriendsManager.Application.Repositories;
+
+public interface IFriendRepository
+{
+    Task CreateAsync(Friend friend);
+    Task DeleteAsync(Friend friend);
+    Task<IEnumerable<Friend>> ReturnAsync();
+    Task<Friend?> ReturnAsync(Guid id);
+    Task UpdateAsync(Friend friend);
+}

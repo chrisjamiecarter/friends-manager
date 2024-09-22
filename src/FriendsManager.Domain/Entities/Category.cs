e0f0f365dac5392d353b0568e5bec8d2a70dad48
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-22 16:22:05 +0100

    Added Domain Entities

---
+++ src/FriendsManager.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace FriendsManager.Domain.Entities;
+
+/// <summary>
+/// Represents a Category entity within the Domain layer.
+/// </summary>
+public class Category
+{
+    #region Properties
+
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = "";
+
+    #endregion
+}
---
+++ src/FriendsManager.Domain/Entities/Friend.cs
@@ -0,0 +1,24 @@
+﻿namespace FriendsManager.Domain.Entities;
+
+/// <summary>
+/// Represents a Friend entity within the Domain layer.
+/// </summary>
+public class Friend
+{
+    #region Properties
+
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = "";
+
+    public int DesiredContactFrequency { get; set; }
+
+    public DateTime LastContactDate { get; set; }
+
+    public string LastContactType { get; set; } = "";
+
+    public Category? Category { get; set; }
+
+    #endregion
+}
+

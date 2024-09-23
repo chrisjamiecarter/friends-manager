126d590437046355b4353a267f52c9408462db12
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-23 19:05:53 +0100

    Added Infrastructure data models

---
+++ src/FriendsManager.Infrastructure/Models/CategoryModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FriendsManager.Infrastructure.Models;
+
+/// <summary>
+/// Represents a Category entity in the infrastructure layer.
+/// </summary>
+[Table("Category")]
+internal class CategoryModel
+{
+    #region Constructors
+
+    #endregion
+    #region Properties
+
+    [Key]
+    public required Guid Id { get; set; }
+
+    [Required]
+    public required string Name { get; set; }
+
+    public required List<FriendModel> Friends { get; set; }
+
+    #endregion
+    #region Methods
+
+    #endregion
+}

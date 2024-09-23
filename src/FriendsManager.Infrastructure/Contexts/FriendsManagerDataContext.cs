5e2b33c9e42f7b9244f23717383b70cf3f9dfbf1
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-23 19:08:57 +0100

    Added EF Packages and Data Context

---
+++ src/FriendsManager.Infrastructure/Contexts/FriendsManagerDataContext.cs
@@ -0,0 +1,23 @@
+﻿using FriendsManager.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Budget.Infrastructure.Contexts;
+
+/// <summary>
+/// Represents the Entity Framework Core database context for the FriendsManager data store.
+/// </summary>
+internal class FriendsManagerDataContext : DbContext
+{
+    #region Constructors
+
+    public FriendsManagerDataContext(DbContextOptions<FriendsManagerDataContext> options) : base(options) { }
+
+    #endregion
+    #region Properties
+
+    public DbSet<CategoryModel> Category { get; set; }
+
+    public DbSet<FriendModel> Friend { get; set; }
+
+    #endregion
+}

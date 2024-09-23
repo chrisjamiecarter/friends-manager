e9d4b6474b2d333090e46e553b5d7c8d23518e06
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-23 20:00:32 +0100

    Implemented the repositories and unit of work

---
+++ src/FriendsManager.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq.Expressions;
+using FriendsManager.Application.Repositories;
+using FriendsManager.Domain.Entities;
+using FriendsManager.Infrastructure.Contexts;
+using FriendsManager.Infrastructure.Extensions;
+using FriendsManager.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Budget.Infrastructure.Repositories;
+
+/// <summary>
+/// Provides repository operations for managing the Infrastructure layer's Category entity.
+/// This class implements the <see cref="ICategoryRepository"/> interface, offering 
+/// methods to perform CRUD operations against the database using Entity Framework Core.
+/// </summary>
+internal class CategoryRepository : ICategoryRepository
+{
+    #region Fields
+
+    private readonly FriendsManagerDataContext _dataContext;
+
+    #endregion
+    #region Constructors
+
+    public CategoryRepository(FriendsManagerDataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    #endregion
+    #region Methods
+
+    public async Task CreateAsync(Category entity)
+    {
+        await _dataContext.Category.AddAsync(entity.ToModel());
+    }
+
+    public async Task DeleteAsync(Category entity)
+    {
+        var model = await _dataContext.Category.FindAsync(entity.Id);
+        if (model is not null)
+        {
+            _dataContext.Category.Remove(model);
+        }
+    }
+
+    public async Task<IEnumerable<Category>> ReturnAsync()
+    {
+        var data = await _dataContext.Category.ToListAsync();
+        return data.Select(x => x.ToDomain());
+    }
+
+    public async Task<Category?> ReturnAsync(Guid id)
+    {
+        var model = await _dataContext.Category.FindAsync(id);
+        return model?.ToDomain();
+    }
+
+    public async Task UpdateAsync(Category entity)
+    {
+        var model = await _dataContext.Category.FindAsync(entity.Id);
+        if (model is not null)
+        {
+            model.Name = entity.Name;
+
+            _dataContext.Category.Update(model);
+        }
+    }
+
+    #endregion
+}
---
+++ src/FriendsManager.Infrastructure/Repositories/FriendRepository.cs
@@ -0,0 +1,85 @@
+﻿using System.Linq.Expressions;
+using FriendsManager.Application.Repositories;
+using FriendsManager.Domain.Entities;
+using FriendsManager.Infrastructure.Contexts;
+using FriendsManager.Infrastructure.Extensions;
+using FriendsManager.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FriendsManager.Infrastructure.Repositories;
+
+/// <summary>
+/// Provides repository operations for managing the Infrastructure layer's Friend entity.
+/// This class implements the <see cref="IFriendRepository"/> interface, offering 
+/// methods to perform CRUD operations against the database using Entity Framework Core.
+/// </summary>
+internal class FriendRepository : IFriendRepository
+{
+    #region Fields
+
+    private readonly FriendsManagerDataContext _dataContext;
+
+    #endregion
+    #region Constructors
+
+    public FriendRepository(FriendsManagerDataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    #endregion
+    #region Methods
+
+    public async Task CreateAsync(Friend entity)
+    {
+        await _dataContext.Friend.AddAsync(entity.ToModel());
+    }
+
+    public async Task DeleteAsync(Friend entity)
+    {
+        var model = await _dataContext.Friend.FindAsync(entity.Id);
+        if (model is not null)
+        {
+            _dataContext.Friend.Remove(model);
+        }
+    }
+
+    public async Task<IEnumerable<Friend>> ReturnAsync()
+    {
+        var data = await _dataContext.Friend.ToListAsync();
+        return data.Select(x => x.ToDomain());
+    }
+
+    public async Task<Friend?> ReturnAsync(Guid id)
+    {
+        var model = await _dataContext.Friend.FindAsync(id);
+
+        if (model is not null && model.Category is null)
+        {
+            var category = await _dataContext.Category.FindAsync(model.CategoryId);
+            if (category is not null)
+            {
+                model.Category = category;
+            }
+        }
+
+        return model?.ToDomain();
+    }
+
+    public async Task UpdateAsync(Friend entity)
+    {
+        var model = await _dataContext.Friend.FindAsync(entity.Id);
+        if (model is not null)
+        {
+            model.Name = entity.Name;
+            model.DesiredContactFrequency = entity.DesiredContactFrequency;
+            model.LastContactDate = entity.LastContactDate;
+            model.LastContactType = entity.LastContactType;
+            model.CategoryId = entity.Category.Id;
+
+            _dataContext.Friend.Update(model);
+        }
+    }
+
+    #endregion
+}

b0ff1b2c369cca98adc11355f6b22d67e5154209
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-24 17:29:47 +0100

    Added API dependeny injection Installer

---
+++ src/FriendsManager.Api/Installers/Installer.cs
@@ -0,0 +1,49 @@
+﻿using FriendsManager.Infrastructure.Installers;
+
+namespace FriendsManager.Api.Installers;
+
+/// <summary>
+/// Registers dependencies and adds any required middleware for the Api layer.
+/// </summary>
+public static class Installer
+{
+    public static IServiceCollection AddApi(this IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+
+    public static WebApplication AddMiddleware(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseCors(options =>
+        {
+            options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        });
+
+        app.MapControllers();
+                
+        return app;
+    }
+
+    public static WebApplication SetUpDatabase(this WebApplication app)
+    {
+        // Performs any database migrations and seeds the database.
+        using var scope = app.Services.CreateScope();
+        var services = scope.ServiceProvider;
+        services.SeedDatabase();
+
+        return app;
+    }
+}

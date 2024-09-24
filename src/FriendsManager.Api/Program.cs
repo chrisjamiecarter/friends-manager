8f590773eb329a54fe6dfd8d959ae929a2136551
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-24 17:30:16 +0100

    Configured app startup

--- src/FriendsManager.Api/Program.cs
+++ src/FriendsManager.Api/Program.cs
@@ -1,3 +1,6 @@
+using FriendsManager.Api.Installers;
+using FriendsManager.Application.Installers;
+using FriendsManager.Infrastructure.Installers;
 
 namespace FriendsManager.Api;
 
@@ -6,29 +9,13 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
+        builder.Services.AddApi();
+        builder.Services.AddApplication();
+        builder.Services.AddInfrastructure(builder.Configuration);
+                
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
+        app.AddMiddleware();
+        app.SetUpDatabase();
 
         app.Run();
     }

43da2b3495beb88c959954e421f49adc893dd1fc
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-26 12:48:02 +0100

    Implemented actual backend API values

--- src/friendsmanager.web/src/services/api.js
+++ src/friendsmanager.web/src/services/api.js
@@ -1,7 +1,8 @@
-const apiUrl = process.env.REACT_APP_API_URL;
+const categoriesApiUrl = process.env.REACT_APP_CATEGORIES_API_URL;
+const friendsApiUrl = process.env.REACT_APP_FRIENDS_API_URL;
 
 export const loadFriends = async () => {
-    return await fetch(apiUrl)
+    return await fetch(friendsApiUrl)
     .then(response => {
         if (response.ok) {
             return response.json();
@@ -15,7 +16,7 @@ export const loadFriends = async () => {
 };
 
 export const addFriend = async (friend) => {
-    return await fetch(apiUrl, {
+    return await fetch(friendsApiUrl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'

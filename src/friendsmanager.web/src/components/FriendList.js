5679e85d99e9add898c0fa78abdc318e17082859
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-28 21:25:03 +0100

    Removed header

--- src/friendsmanager.web/src/components/FriendList.js
+++ src/friendsmanager.web/src/components/FriendList.js
@@ -17,7 +17,6 @@ const FriendList = () => {
 
     return (
         <div className='friends-container'>
-            <h2 className='text-center text-lg font-semibold text-slate-800 capitalize my-4'>Friends</h2>
             <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
                 {friends && friends.map((friend) => (
                     <FriendCard key={friend.id} friend={friend} />

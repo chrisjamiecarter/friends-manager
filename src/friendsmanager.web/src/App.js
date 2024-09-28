5c89120c999e0928cc2a2434c24c9bc243837bc2
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-28 21:31:48 +0100

    Removed reference to App.css

--- src/friendsmanager.web/src/App.js
+++ src/friendsmanager.web/src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 import Header from './components/Header';
 import AddFriendForm from './components/AddFriendForm';
 import FriendList from './components/FriendList';
